62d2cdf0d529d420e305c553db910a9f85065236
CJ3022196 <CJ3022196@LAB01-PC39>
2024-03-06 08:55:22 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3022196/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program 
+{
+  static void Main()
+    {
+        Console.WriteLine("Digite sua nota");
+        double nota =
+         Convert.ToDouble(Console.ReadLine());
+
+        //Verificação se a nota é maior de 6
+
+        if (nota >=6)
+        {
+            Console.WriteLine("O aluno passou!");
+        }
+        else
+        {
+            Console.WriteLine("O aluno não passou");
+        }
+
+    }
+}
+
+
